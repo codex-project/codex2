b9ba3fdb44c6b18086576594710d4ed7f2903642
Robin Radic <robin@radic.nl>
2015-08-03 04:33:11 +0200

    init

---
+++ src/CodexServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Part of  Robin Radic's PHP packages.
+ *
+ * MIT License and copyright information bundled with this package
+ * in the LICENSE file or visit http://radic.mit-license.org
+ */
+namespace Codex\Codex;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * {@inheritdoc}
+ */
+class CodexServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+    }
+}
